2d95953a27613e2fe994fae5c59ed1baa8cb860b
Chris Joakim <christopher.joakim@gmail.com>
2021-02-11 13:11:13 -0500

    added code/rest/ dir

---
+++ code/rest/pyenv.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Bash script to create, populate, and activate the python virtual environment
+# for this project with pyenv.
+# Chris Joakim, 2021/02/11
+
+# These are the only two values that need to change between projects:
+venv_name="cosmosrest"
+python_version="3.8.6"  # 3.7.9
+
+echo '=== creating virtualenv '$venv_name
+rm .python-version
+pyenv virtualenv -f $python_version $venv_name
+
+echo '=== python version'
+python --version 
+
+echo '=== setting pyenv local ...'
+pyenv local $venv_name
+
+echo '=== upgrade pip ...'
+pip install --upgrade pip
+
+echo '=== install pip-tools ...'
+pip install pip-tools
+
+echo '=== pip compile ...'
+pip-compile
+
+echo '=== pip install ...'
+pip install -r requirements.txt
+
+echo '=== pip list ...'
+pip list
+
+echo '=== .python-version ...'
+cat .python-version
+
+echo 'done'

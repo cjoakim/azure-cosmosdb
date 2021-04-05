68f94297be0d65911b31293cf283fe24b5f554a3
Chris Joakim <christopher.joakim@gmail.com>
2021-04-05 05:24:29 -0400

    Created code/python/apps/composite_indexes/load1.sh and load2.sh for concurrent loading

--- code/python/apps/composite_indexes/load2.sh
+++ code/python/apps/composite_indexes/load2.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Bash script to load the two containers with the same data, but different indexing.
+# Bash script to load the postalcodes2 container.
 # Chris Joakim, 2021/04/05
 
 infile="data/postal_codes_us_filtered.csv"
-
-python main.py load_container dev postalcodes1 $infile 0 99999 > tmp/load_postalcodes1.txt
 
 python main.py load_container dev postalcodes2 $infile 0 99999 > tmp/load_postalcodes2.txt
 

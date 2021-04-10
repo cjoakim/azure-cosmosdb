e5f9f52ef48e5eeca656d766a45b6134df0ab1f8
Chris Joakim <christopher.joakim@gmail.com>
2021-04-10 06:47:57 -0400

    Added code/dotnet/composite_indexes/ - wip

---
+++ code/dotnet/composite_indexes/PostalCodeMap.cs
@@ -0,0 +1,25 @@
+
+using CsvHelper.Configuration;
+
+// Mapping class for use with the CsvHelper library; maps csv header to object names.
+// See https://joshclose.github.io/CsvHelper/getting-started/#reading-a-csv-file
+//
+// Chris Joakim, Microsoft, 2021/04/08
+
+namespace CJoakim.Cosmos.BulkLoader
+{
+    public class PostalCodeMap : ClassMap<PostalCode>
+    {
+        public PostalCodeMap()
+        {
+            // rowId,postalCode,countryCode,cityName,stateAbbrv,latitude,longitude
+            Map(m => m.rowId).Name("rowId");
+            Map(m => m.postalCode).Name("postalCode");
+            Map(m => m.countryCode).Name("countryCode");
+            Map(m => m.cityName).Name("cityName");
+            Map(m => m.stateAbbrv).Name("stateAbbrv");
+            Map(m => m.latitude).Name("latitude");
+            Map(m => m.longitude).Name("longitude");
+        }
+    }
+}

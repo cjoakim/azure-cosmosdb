283dcb4bf2fbdcb4ff0237393fee8560bda60202
Chris Joakim <christopher.joakim@gmail.com>
2021-05-06 05:38:33 -0400

    code/dotnet/storage_client/ - wip

---
+++ code/dotnet/storage_client/Config.cs
@@ -0,0 +1,154 @@
+namespace storage_client
+{
+    using System;
+    using Newtonsoft.Json;
+
+    /**
+     * This class is the source of all configuration values for this application -  
+     * including environment variables and command-line arguments.
+     */
+
+    public class Config
+    {
+        // Constants; environment variables:
+        public const string AZURE_STORAGE_CONNECTION_STRING = "AZURE_STORAGE_CONNECTION_STRING";
+        public const string AZURE_STORAGE_ACCOUNT           = "AZURE_STORAGE_ACCOUNT";
+        public const string AZURE_STORAGE_KEY               = "AZURE_STORAGE_KEY";
+
+        // Constants; command-line and keywords:
+        public const string CLI_ARGS_STRING                 = "CLI_ARGS_STRING";  // for executing in a Docker container
+        public const string CLI_FUNCTION_LIST_CONTAINER     = "list_container";
+        public const string CLI_FUNCTION_STREAM             = "stream";
+        public const string CONTAINER_KEYWORD               = "--container";
+        public const string BLOB_KEYWORD                    = "--blob";
+        public const string VERBOSE_FLAG                    = "--verbose";
+        
+        // Instance variables:
+        private string[] cliArgs = { };
+
+        public Config(string[] args)
+        {
+            cliArgs = args;  // dotnet run xxx yyy -> args:["xxx","yyy"]
+            Console.WriteLine($"Config args: {args}");
+            
+            if (cliArgs.Length == 0)
+            {
+                // If no args, then the Program was invoked in a Docker container, 
+                // so use the CLI_ARGS_STRING environment variable instead.
+                cliArgs = GetEnvVar(CLI_ARGS_STRING, "").Split();
+                Console.WriteLine("CLI_ARGS: " + JsonConvert.SerializeObject(cliArgs));
+            }
+        }
+
+        public bool IsValid()
+        {
+            Console.WriteLine("Config#IsValid args: " + JsonConvert.SerializeObject(cliArgs));
+
+            if (cliArgs.Length < 2)
+            {
+                Console.WriteLine("ERROR: empty command-line args");
+                return false;
+            }
+            // switch (fileType)
+            // {
+            //     case FILE_TYPE_VERTEX:
+            //         break;
+            //     case FILE_TYPE_EDGE:
+            //         break;
+            //     default:
+            //         Console.WriteLine("Invalid Config: unknown file type {0}", fileType);
+            //         return false;
+            // }
+
+            return true;
+        }
+
+        public string[] GetCliArgs()
+        {
+            return cliArgs;
+        }
+
+        public string GetRunType()
+        {
+            return cliArgs[0].ToLower();
+        }
+        
+        /**
+         * This method is intended for unit-testing purposes only; see ConfigTest.cs
+         */
+        public void SetCliArgs(string commandLine)
+        {
+            cliArgs = commandLine.Split(" ");
+        }
+
+        public string GetStorageConnString()
+        {
+            return GetEnvVar(AZURE_STORAGE_CONNECTION_STRING, null);
+        }
+
+        public string GetEnvVar(string name)
+        {
+            return Environment.GetEnvironmentVariable(name);
+        }
+        
+        public string GetEnvVar(string name, string defaultValue=null)
+        {
+            string value = Environment.GetEnvironmentVariable(name);
+            if (value == null)
+            {
+                return defaultValue;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public bool IsVerbose()
+        {
+            for (int i = 0; i < cliArgs.Length; i++)
+            {
+                if (cliArgs[i] == VERBOSE_FLAG)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetContainerName()
+        {
+            return GetCliKeywordArg(CONTAINER_KEYWORD);
+        }
+        
+        public string GetBlobName()
+        {
+            return GetCliKeywordArg(BLOB_KEYWORD);
+        }
+        public string GetCliKeywordArg(string keyword, string defaultValue=null)
+        {
+            try
+            {
+                for (int i = 0; i < cliArgs.Length; i++)
+                {
+                    if (keyword == cliArgs[i])
+                    {
+                        return cliArgs[i + 1];
+                    }
+                }
+                return defaultValue;
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Config:");
+            Console.WriteLine($"  args:            {JsonConvert.SerializeObject(GetCliArgs())}");
+            Console.WriteLine($"  run type:        {GetRunType()}");
+        }
+    }
+}
--- code/dotnet/storage_client/Program.cs
+++ code/dotnet/storage_client/Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+// dotnet run list_container --container imdb 
 
 namespace storage_client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static Config config = null;
+        private static BlobServiceClient blobServiceClient = null;
+        private static BlobContainerClient containerClient = null;
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            config = new Config(args);
+            Console.WriteLine("Hello Storage Client!");
+
+            switch (config.GetRunType())
+            {
+                case Config.CLI_FUNCTION_LIST_CONTAINER:
+                    listContainer();
+                    break;
+                case Config.CLI_FUNCTION_STREAM:
+                    streamBlob();
+                    break;
+                default:
+                    Console.WriteLine($"Invalid Config: unknown file run type {config.GetRunType()}");
+                    break;
+            }
+        }
+
+        private static async Task listContainer()
+        {
+            connectToStorageContainer();
+            
+            await foreach (BlobItem blob in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine($"blob: {blob.Name} {blob.Properties}");
+            }
+        }
+        
+        private static async Task streamBlob()
+        {
+            connectToStorageContainer();
+        }
+
+        private static async Task connectToStorageContainer()
+        {
+            string connStr = config.GetStorageConnString();
+            Console.WriteLine($"connection string: {connStr}");
+            blobServiceClient = new BlobServiceClient(connStr);
+            Console.WriteLine($"account: {blobServiceClient.AccountName}");
+
+            string cName = config.GetContainerName();
+            Console.WriteLine($"container name: {cName}");
+            containerClient = await blobServiceClient.CreateBlobContainerAsync(cName);
+            Console.WriteLine($"container uri: {containerClient.Uri}");
         }
     }
 }

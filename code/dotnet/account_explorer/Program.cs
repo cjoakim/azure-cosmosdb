e9dd4328539211074623acc05918d41fb12930e6
Chris Joakim <christopher.joakim@gmail.com>
2021-05-14 16:28:39 -0400

    code/dotnet/account_explorer/Program.cs - tasks loop

--- code/dotnet/account_explorer/Program.cs
+++ code/dotnet/account_explorer/Program.cs
@@ -60,6 +60,27 @@ namespace account_explorer
         private static async Task Tasks()
         {
             Console.WriteLine("Tasks");
+
+            int batchCount = 5;
+            int taskCount  = 10;
+            Random random = new Random();
+            List<Task> tasks = null;
+
+            for (int b = 0; b < batchCount; b++)
+            {
+                long startTime = EpochMsTime();
+                Console.WriteLine($"batch {b} adding tasks at {startTime}");
+                tasks = new List<Task>();
+                for (int t = 0; t < taskCount; t++)
+                {
+                    int ms = random.Next(3000, 5000);
+                    Console.WriteLine($"batch {b} adding task {ms}");
+                    tasks.Add(Task.Delay(ms));
+                }
+                await Task.WhenAll(tasks.ToArray());
+                long finishTime = EpochMsTime();
+                Console.WriteLine($"batch {b} finished tasks at {finishTime} ({finishTime - startTime})");
+            }
             await Task.Delay(0);
         }
         
@@ -88,6 +109,11 @@ namespace account_explorer
 
             container = await db.CreateContainerIfNotExistsAsync(collName, standardPartitionKeyName);
             Console.WriteLine("connected to container: {0}", container.Id);
+        }
+        
+        private static long EpochMsTime()
+        {
+            return new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
         }
     }
 }

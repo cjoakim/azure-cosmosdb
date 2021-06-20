00ecf4271fe901cae0dc29468145247082aaf6aa
Chris Joakim <christopher.joakim@gmail.com>
2021-06-20 08:00:51 -0400

    mdgen

--- presentations/mdgen.py
+++ presentations/mdgen.py
@@ -16,6 +16,51 @@ import jinja2
 # Class MarkdownGenerator was used to generate the initial set of documentation
 # markdown files.
 
+
+class Base(object):
+
+    def __init__(self, line):
+        self.line = line
+        self.tokens = tokens.split()
+
+class Section(Base):
+
+    def __init__(self, line):
+        Base.__init__(self, line)
+
+    def is_section(self):
+        return True
+
+    def is_page(self):
+        return False
+
+
+class Page(Base):
+
+    def __init__(self, line, tokens):
+        Base.__init__(self, line)
+
+    def page(self, line_idx, csv_line):
+        tokens = csv_line.split(',')
+        page_num = '{:02d}'.format(line_idx)
+        page_name = tokens[0]
+        page = {}
+        page['num'] = page_num
+        page['name'] = page_name
+        page['filename'] = '{}_{}.md'.format(page_num, page_name).lower().replace(' ','_')
+        page['sections'] = list()
+        for idx, tok in enumerate(tokens):
+            if idx > 0:
+                page['sections'].append(tok.strip())
+        return page
+
+    def is_section(self):
+        return False
+
+    def is_page(self):
+        return True
+
+
 class MarkdownGenerator(object):    
 
     def __init__(self):
@@ -41,58 +86,66 @@ class MarkdownGenerator(object):
 
     def documentation_structure(self):
         lines, structure = list(), list()
-        lines.append('Cloud')
+        lines.append('Section,1,Cloud')
         lines.append('Azure,IaaS,PaaS,Costs,Innovation,Provisioning,Pets vs Cattle,DevOps,Security,Marketplace')
         lines.append('NoSQL,Know SQL,Definition,Spectrum')
 
-        lines.append('CosmosDB Overview')
+        lines.append('Section,2,,CosmosDB Overview')
         lines.append('CosmosDB APIs,Family of Databases,SQL(Document),Mongo,Cassandra,Gremlin/Graph,Table')
-        
-        lines.append('CosmosDB Features')
-        lines.append('CosmosDB Basics,PaaS Service,JSON,Schemaless')
+        lines.append('Cost Model')
+
+        lines.append('Section,3,CosmosDB Features')
+        lines.append('CosmosDB Basics,PaaS Service,JSON,Schemaless,')
         lines.append('CosmosDB Non-Features,Joins,Referential Integrity,Identity Columns')
         lines.append('Partitioning,Partition Key,Physical Partitions,Limits')
         lines.append('Request Units,Definition,Provisioned,Autoscale,Serverless')
-        lines.append('Multi-Region,Consistency Levels,SDKs')
+        lines.append('Multi-Region,Consistency Levels,SDKs,Uptime SLA,Performance SLA')
+        lines.append('SQL,Examples')
         lines.append('Server Side Programming,Stored Procedures,UDFs,Triggers')
         lines.append('Change-Feed,Azure Functions,Pipelines')
         lines.append('TTL,Benefits,Redis')
         lines.append('Spatial Support,GeoJson')
         lines.append('Azure Monitor,Kusto')
 
-        lines.append('Design')
-        lines.append('Relational-to-Cosmos,iPhone,Retail Examples,Adventureworks')
-        lines.append('Design,Containers,Partition Key,Indexing')
+        lines.append('Section,4,Design and Development')
+        lines.append('Design Process,Query Driven,RU costs,Excel,Kusto,Iterations')
+        lines.append('Design Considerations,Containers,Partition Key,Indexing,Composite Indexes')
+        lines.append('Relational-to-Cosmos Example,iPhone,Retail Examples,Adventureworks')
+        lines.append('Local Development,Windows Emulator,Linux Emulator,Mongo Docker,Serverless')
 
-        lines.append('Integrations')
-        lines.append('Synapse Link,Synapse,Storage/Parquet,PySpark Example')
+        lines.append('Section,5,Integrations')
         lines.append('Azure Stream Analytics')
+        lines.append('Azure Functions')
+        lines.append('Synapse Link,Synapse,Storage/Parquet,PySpark Example')
         lines.append('Azure Search')
-        lines.append('Azure Databricks')
+        lines.append('Azure Databricks,https://devblogs.microsoft.com/cosmosdb/spark-3-connector-databricks')
+        lines.append('Applications,App Service,Azure Kubernetes Service')
+        lines.append('Kafka,https://devblogs.microsoft.com/cosmosdb/seamlessly-transfer-kafka-data-with-the-new-azure-cosmos-db-connector-for-confluent/')
         lines.append('PowerBI')
-        lines.append('')
-        lines.append('')
-        lines.append('')
-        lines.append('')
-        lines.append('')
-        
-        for line_idx, line in enumerate(lines):
-            structure.append(self.page(line_idx, line))
-        return structure
 
-    def page(self, line_idx, csv_line):
-        tokens = csv_line.split(',')
-        page_num = '{:02d}'.format(line_idx)
-        page_name = tokens[0]
-        page = {}
-        page['num'] = page_num
-        page['name'] = page_name
-        page['filename'] = '{}_{}.md'.format(page_num, page_name).lower().replace(' ','_')
-        page['sections'] = list()
-        for idx, tok in enumerate(tokens):
-            if idx > 0:
-                page['sections'].append(tok.strip())
-        return page
+        lines.append('Section,6,New and Preview Features')
+        lines.append('New and Preview Features,Point-in-Time Restore,Partial Document Update,Other')
+        # lines.append('Point-in-Time Restore,https://docs.microsoft.com/en-us/azure/cosmos-db/continuous-backup-restore-portal')
+        # lines.append('Partial Document Update,https://azure.microsoft.com/en-us/updates/partial-document-update-for-azure-cosmos-db-in-private-preview/')
+
+        lines.append('Section,7,Other CosmosDB APIs')
+        lines.append('Gremlin-Graph,Apache Tinkerpop,Bill-of-Material Example,Bulk Loader')
+        lines.append('MongoDB,Migration')
+        lines.append('Cassandra')
+        lines.append('Table')
+        lines.append('Future API1')
+        lines.append('Future API2')
+        lines.append('Future API3')
+
+        lines.append('Section,8,Summary')
+        lines.append('Thank you,Contact Info')
+
+        for line_idx, line in enumerate(lines):
+            if line.lower().startswith('section,'):
+                structure.append(Section(line))
+            else:
+                structure.append(Page(line))
+        return structure
 
     def timestamp(self):
         return arrow.utcnow().format('YYYY-MM-DD HH:mm:ss UTC')

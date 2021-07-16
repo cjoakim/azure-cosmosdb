8073e1b4b6be2dfbe642a5f10a693012f2fc15a1
Chris Joakim <christopher.joakim@gmail.com>
2021-07-16 12:53:11 -0400

    code/dotnet/mongo_loader/ - loading locally

--- code/dotnet/mongo_loader/Program.cs
+++ code/dotnet/mongo_loader/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using MongoDB.Driver;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 // See https://mongodb.github.io/mongo-csharp-driver/2.12/getting_started/quick_tour/
 
@@ -13,12 +18,15 @@ namespace mongo_loader {
         private static string infile   = null;
         private static string target   = "none";
         private static bool   verbose  = false;
+        private static bool   createNewDocIds = false;
         private static int    lineNumber = 0;
         private static long   tracerInterval = 10000;
         private static int    rowMaxRetries = 20;
         private static long   totalRetryCount = 0;
+        private static long   totalFailureCount = 0;
         private static MongoClient client = null;
-
+        private static dynamic databaseObj = null;
+        private static dynamic collectionObj = null;
         
         static void Main(string[] args) {
             cliArgs = args;
@@ -32,10 +40,12 @@ namespace mongo_loader {
             try {
                 client = CreateMongoClient();
                 if (client != null) {
-                    Console.WriteLine($"target: {target}, client: {client}, classname: {client.GetType().FullName}");
+                    Console.WriteLine($"MongoClient create for target: {target}");
+                    databaseObj = client.GetDatabase(dbname);  //  MongoDB.Driver.MongoDatabaseImpl
+                    collectionObj = databaseObj.GetCollection<BsonDocument>(collname);  // MongoDB.Driver.MongoCollectionImpl
                 }
                 else {
-                    Console.WriteLine($"client is null for target: {target}");
+                    Console.WriteLine($"MongoClient is null for target: {target}");
                 }
 
                 ProcessingLoop();
@@ -53,7 +63,8 @@ namespace mongo_loader {
                 collname = cliArgs[1];
                 infile   = cliArgs[2];
                 // Keyword args
-                foreach (string arg in cliArgs) {
+                for (int i = 0; i < cliArgs.Length; i++) {
+                    string arg = cliArgs[i];
                     Console.WriteLine($"arg: {arg}");
                     if (arg.Equals("--targetLocal")) {
                         target = "local";
@@ -67,13 +78,32 @@ namespace mongo_loader {
                     if (arg.Equals("--quiet")) {
                         verbose = false;
                     }
+                    if (arg.Equals("--createNewDocIds")) {
+                        createNewDocIds = true;
+                    }
+                    if (arg.Equals("--tracerInterval")) {
+                        tracerInterval = Int32.Parse(cliArgs[i + 1]);
+                    }
+                    if (arg.Equals("--rowMaxRetries")) {
+                        rowMaxRetries = Int32.Parse(cliArgs[i + 1]);
+                    }
                 }
+                
+                Console.WriteLine("Args used this run:"); 
+                Console.WriteLine($"  dbname:          {dbname}");
+                Console.WriteLine($"  collname:        {collname}");
+                Console.WriteLine($"  infile:          {infile}");
+                Console.WriteLine($"  target:          {target}");
+                Console.WriteLine($"  verbose:         {verbose}");
+                Console.WriteLine($"  createNewDocIds: {createNewDocIds}");
+                Console.WriteLine($"  tracerInterval:  {tracerInterval}");
+                Console.WriteLine($"  rowMaxRetries:   {rowMaxRetries}");
                 return true;
             }
             else {
                 Console.WriteLine("Invalid command-line args, program exiting:");
-                Console.WriteLine("  source env.sh ; dotnet run <dbname> <collname> <infile> [--targetLocal|--targetCosmos] [--verbose|--quiet]");
-                Console.WriteLine("  source env.sh ; dotnet run openflights planes data/openflights__routes.json --targetLocal");
+                Console.WriteLine("  source env.sh ; dotnet run <dbname> <collname> <infile> [--targetLocal|--targetCosmos] [--verbose|--quiet] --createNewDocIds --tracerInterval x --rowMaxRetries y");
+                Console.WriteLine("  source env.sh ; dotnet run openflights planes data/openflights__routes.json --targetLocal --createNewDocIds --tracerInterval 1000 --rowMaxRetries 7");
                 return false;
             }
         }
@@ -105,42 +135,75 @@ namespace mongo_loader {
 
         private static void ProcessingLoop() {
             long loopStartEpochMs = EpochMsTime();
+            LogLineInterval("start", loopStartEpochMs);
             System.IO.StreamReader file = new System.IO.StreamReader(infile);
             string line = null;
+
             while ((line = file.ReadLine()) != null) {
                 long lineStartEpochMs = EpochMsTime();
                 lineNumber++;
                 for (int i = 0; i < rowMaxRetries; i++) {
-                    bool successful = ProcessLine(line.Trim());
+                    bool successful = ProcessLine(collectionObj, line.Trim());
                     if (successful) {
-                        long lineElapsedMs = EpochMsTime() - lineStartEpochMs;
                         if (verbose) {
+                            long lineElapsedMs = EpochMsTime() - lineStartEpochMs;
                             Console.WriteLine(
-                                $"successfully processed line: {lineNumber} : {line} : {i} : {lineElapsedMs}");
+                                $"ProcessingLoop - successfully processed line: {lineNumber} : {line} : {i} : {lineElapsedMs}");
                         }
                         break;
                     }
                     totalRetryCount++;
                     Thread.Sleep(1000);
+                    if (i == (rowMaxRetries - 1)) {
+                        totalFailureCount++;
+                    }
                 }
                 
                 if ((lineNumber % tracerInterval) == 0) {
-                    Console.WriteLine($"{lineNumber}");
+                    LogLineInterval("interval", loopStartEpochMs);
                 }
             }
-
-            long loopElapsedMs = EpochMsTime() - loopStartEpochMs;
-            Console.WriteLine($"ProcessingLoop elapsed: {loopElapsedMs}");
+            LogLineInterval("completed", loopStartEpochMs);
+            
+            // ProcessingLoop - completed, line: 67663, minutes: 1.4711166666666666, retries: 0, failures: 0
         }
 
-        private static bool ProcessLine(string line) {
+        private static void LogLineInterval(string state, long loopStartEpochMs) {
+            double elapsedMinutes = ElapsedMsToMinutes(EpochMsTime() - loopStartEpochMs);
+            if (state.Equals("start")) {
+                elapsedMinutes = 0.0;
+            }
+            Console.WriteLine(
+                $"ProcessingLoop - {state}, line: {lineNumber}, minutes: {elapsedMinutes}, retries: {totalRetryCount}, failures: {totalFailureCount}");
+        }
 
-            return true;
+        private static bool ProcessLine(dynamic collectionObj, string line) {
+
+            try {
+                JObject jObj = JObject.Parse(line);
+                if (createNewDocIds) {
+                    jObj.Remove("_id");
+                }
+                var bsonDoc = jObj.ToBsonDocument();
+                if (verbose) {
+                    Console.WriteLine($"ProcessLine, bsonDoc: {bsonDoc}");
+                }
+                collectionObj.InsertOne(bsonDoc);
+                return true;
+            }
+            catch (Exception e) {
+                Console.WriteLine($"ProcessLine, error on line: {lineNumber} : {line}");
+                Console.WriteLine(e);
+                return false;
+            }
         }
         
-        private static long EpochMsTime()
-        {
+        private static long EpochMsTime() {
             return new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+        }
+
+        private static double ElapsedMsToMinutes(long elapsedMs) {
+            return elapsedMs / 60000.0;
         }
     }
 }

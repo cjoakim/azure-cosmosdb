6b39760cafa284bf37b973d59dcb5281a14c4a6a
Chris Joakim <christopher.joakim@gmail.com>
2022-03-22 08:56:51 -0400

    run.ps1

--- code/java/java_simple_cosmos_sql/sample/run.ps1
+++ code/java/java_simple_cosmos_sql/sample/run.ps1
@@ -1,8 +1,7 @@
 
 # Execute the main Program via "mvn exec:java".
-# Chris Joakim, Microsoft, 2021/06/28
-
+# Chris Joakim, Microsoft, 2021/03/22
 
 mvn exec:java `
   -D"exec.mainClass"="org.cjoakim.azure.cosmos.sql.App" `
-  -D"exec.args"="select_distinct dev airports"
+  -D"exec.args"="point_read dev airports SFO 895014e0-1d52-40f6-8ae2-f9dcb0119961"
